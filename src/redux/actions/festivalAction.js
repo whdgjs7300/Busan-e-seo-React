7990e9606d3ce88d57a655ba0053f6b5199d57f0
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-06-04 18:29:59 +0900

    Update festivalAction.js

--- src/redux/actions/festivalAction.js
+++ src/redux/actions/festivalAction.js
@@ -1,6 +1,6 @@
-import api from "../api";
+import api from '../api';
 
-const KEY = '9V%2BSdKNbzQD7oIQPHdDdlKZz0%2BPj1gnzDGKeS%2B8GWk2LHpSkDx5Ig%2F7u6wKopPZEf9brLck%2Bz3z81NapmasU%2Fg%3D%3D';
+const KEY ="9V%2BSdKNbzQD7oIQPHdDdlKZz0%2BPj1gnzDGKeS%2B8GWk2LHpSkDx5Ig%2F7u6wKopPZEf9brLck%2Bz3z81NapmasU%2Fg%3D%3D"
 
 
 function getFestival() {
@@ -8,7 +8,7 @@ function getFestival() {
         try {
             dispach({type : "GET_FESTIVAL_REQUEST" })
 
-        const festivalApi = api.get(`?serviceKey=${KEY}&pageNo=1&numOfRows=10&resultType=json`)
+        const festivalApi = api.get(`http://apis.data.go.kr/6260000/FestivalService/getFestivalKr?serviceKey=${KEY}&pageNo=1&numOfRows=10&resultType=json`)
 
 
         let [festivalList,] = await Promise.all([festivalApi,])
@@ -16,7 +16,7 @@ function getFestival() {
         dispach({
             type : "GET_FESTIVAL_SUCCESS", 
             payload : {
-                festivalList : festivalList.data,
+                festivalList : festivalList.data.getFestivalKr.item,
             }
         })
         }
