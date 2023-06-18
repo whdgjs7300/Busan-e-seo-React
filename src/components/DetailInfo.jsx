ef245c4787cdb3363588a36e74f115f367541c86
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-06-18 15:50:34 +0900

    6/18 detailset

--- src/components/DetailInfo.jsx
+++ src/components/DetailInfo.jsx
@@ -6,7 +6,7 @@ const DetailInfo = ({item}) => {
         <div className="detail_info_box">
             <div className="detail_info_box1">
                 <p>주소 </p> <span>{item.ADDR1 ? item.ADDR1 : item.ADDR2}</span>
-                <p>전화번호</p> <span>{item.CNTCT_TEL}</span>
+                <p>전화번호</p> <span>{item.CNTCT_TEL ? item.CNTCT_TEL : "없 음"}</span>
                 <p>축제기간</p> <span>{item.USAGE_DAY_WEEK_AND_TIME ? item.USAGE_DAY_WEEK_AND_TIME 
                 : item.USAGE_DAY}</span>
                 <p>이용요금</p> <span>{item.USAGE_AMOUNT ? item.USAGE_AMOUNT : "미 정"}</span>
