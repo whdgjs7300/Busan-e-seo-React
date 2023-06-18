8bc0abb556306b162887973f0c389974ad1ef4fb
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-06-18 20:35:16 +0900

    6/18 kakaoMapApi

--- src/components/DetailInfo2.jsx
+++ src/components/DetailInfo2.jsx
@@ -2,11 +2,18 @@
 const DetailInfo2 = ({item}) => {
 
     return ( 
-        <div>
-            <h2>{item.SUBTITLE || item.PLACE}</h2>
+        <div className="info2_container">
+            <h2 style={{
+                fontWeight : "700",
+                marginBottom : "50px"
+        }}>{item.SUBTITLE || item.PLACE}</h2>
             <p>{ // 문자열을 줄바꿈 문자('\n')을 기준으로 분할
             item.ITEMCNTNTS.split('\n').map((line, i) =>
-                <span>{line}
+                <span style={{
+                    fontWeight : "500",
+                    display : "inline-block",
+                    marginTop : "50px"
+                }}>{line}
                 { //현재 처리 중인 line의 인덱스가 문자열을 분할한 후의 마지막 줄의 인덱스와 같지 않을 때
                 i !== item.ITEMCNTNTS.split('\n').length - 1 && <br />}
                 </span>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 
 
-const Map = () => {
+const { kakao } = window;
+
+const Map = ({item}) => {
+
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+		const options = {
+			center: new kakao.maps.LatLng(item.LAT, item.LNG),
+			level: 6,
+            isPanto : true,
+            
+		};
+        const map = new kakao.maps.Map(container, options);
+    }, []);
 
     return ( 
-        <div>
-
+        <div className="map_container">
+            <div id='myMap'  style={{
+            width: '500px', 
+            height: '500px'
+            }}></div>
         </div>
+        
     );
 }
 
