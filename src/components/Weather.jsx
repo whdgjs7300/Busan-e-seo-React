b966fb4f04ef258ae7c9d4bcb193c14bcf83cbd1
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-06-19 19:45:21 +0900

    6/19 weatherAPISet

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,43 @@
+import ClipLoader from "react-spinners/ClipLoader";
+import {useEffect, useState} from 'react';
 
+const WEATHER_KEY ="5e0b6fefbcb22aa7d9f241571b406f98";
 
-const Weather = () => {
+const Weather = ({lat, lon}) => {
 
+    const [weather, setWeather] = useState(null);
+    const [loading,setLoading] = useState(false);
+
+    
+    const getWeather = async(lat, lon) => {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_KEY}&units=metric`
+        setLoading(true);
+        let response = await fetch(url);
+        let data = await response.json();
+        // 날씨 데이터를 넣어줌 state에
+        setWeather(data);
+        setLoading(false);
+    }
+    useEffect(()=>{
+        getWeather(lat, lon);
+    },[])
+    console.log(weather)
     return ( 
+        
         <div>
-
+            {
+                loading ? (
+                    <ClipLoader
+                        color="#f88c6b"
+                        loading={loading}
+                        size={150}
+                    /> 
+                ) : (
+                    <div>
+                        
+                    </div>
+                )
+            }
         </div>
     );
 }
--- src/redux/actions/detailAction.js
+++ src/redux/actions/detailAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const KEY ="9V%2BSdKNbzQD7oIQPHdDdlKZz0%2BPj1gnzDGKeS%2B8GWk2LHpSkDx5Ig%2F7u6wKopPZEf9brLck%2Bz3z81NapmasU%2Fg%3D%3D"
-const WEATHER_KEY ="5e0b6fefbcb22aa7d9f241571b406f98";
+
 
 
 function getDetail(id, lat, lon) {
@@ -11,11 +11,11 @@ function getDetail(id, lat, lon) {
             dispach({type : "GET_DETAIL_REQUEST" })
 
         const fesDetailApi = axios.get(`http://apis.data.go.kr/6260000/FestivalService/getFestivalKr?serviceKey=${KEY}&pageNo=1&numOfRows=10&resultType=json&UC_SEQ=${id}`)
-       //const weatherApi = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_KEY}`)
+        
 
         let [fesDetailList, ] = await Promise.all([
             fesDetailApi, ])
-
+            
         dispach({
             type : "GET_DETAIL_SUCCESS", 
             payload : {
